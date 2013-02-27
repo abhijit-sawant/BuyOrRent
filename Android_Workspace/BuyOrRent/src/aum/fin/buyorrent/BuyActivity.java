b41ae0bcf811cc03a0a71858a1eeb135b5eeb596
Abhijit Sawant <abhijit.abhi1980@gmail.com>
2013-02-26 21:32:21 -0500

    UI changes to activity_buy
    Made font size of all inpute labels small. Added prefix actBuy_ to all
    UI Ids.

--- Android_Workspace/BuyOrRent/src/aum/fin/buyorrent/BuyActivity.java
+++ Android_Workspace/BuyOrRent/src/aum/fin/buyorrent/BuyActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.app.Activity;
 
@@ -17,9 +16,9 @@ public class BuyActivity extends Activity {
         setContentView(R.layout.activity_buy);
         
         //yearly expenses
-        final RelativeLayout loYearlyTitle   = (RelativeLayout) findViewById(R.id.layoutYearlyTitle);
-    	final RelativeLayout loYearlyContent = (RelativeLayout) findViewById(R.id.layoutYearlyContent);
-    	final ImageView      imgYearlyArrow  = (ImageView) findViewById(R.id.imageYearlyArrow);
+        final RelativeLayout loYearlyTitle   = (RelativeLayout) findViewById(R.id.actBuy_layoutYearlyTitle);
+    	final RelativeLayout loYearlyContent = (RelativeLayout) findViewById(R.id.actBuy_layoutYearlyContent);
+    	final ImageView      imgYearlyArrow  = (ImageView) findViewById(R.id.actBuy_imageYearlyArrow);
         loYearlyTitle.setOnClickListener(new OnClickListener() {
         	public void onClick(View v) {
     	    if (v == loYearlyTitle) {
@@ -32,9 +31,9 @@ public class BuyActivity extends Activity {
     	} });
         
         //peripheral expenses
-        final RelativeLayout loPeriTitle   = (RelativeLayout) findViewById(R.id.layoutPeriTitle);
-    	final RelativeLayout loPeriContent = (RelativeLayout) findViewById(R.id.layoutPeriContent);
-    	final ImageView      imgPeriArrow  = (ImageView) findViewById(R.id.imagePeriArrow);
+        final RelativeLayout loPeriTitle   = (RelativeLayout) findViewById(R.id.actBuy_layoutPeriTitle);
+    	final RelativeLayout loPeriContent = (RelativeLayout) findViewById(R.id.actBuy_layoutPeriContent);
+    	final ImageView      imgPeriArrow  = (ImageView) findViewById(R.id.actBuy_imagePeriArrow);
         loPeriTitle.setOnClickListener(new OnClickListener() {
         	public void onClick(View v) {
     	    if (v == loPeriTitle) {
