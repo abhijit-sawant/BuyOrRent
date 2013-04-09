b2b4c2411c0abeb5f92c7953b3c0fcbca5f2e64c
Abhijit Sawant <abhijit.abhi1980@gmail.com>
2013-04-08 22:57:44 -0400

    Refinements in text box and seek bar interaction

--- Android_Workspace/BuyOrRent/src/aum/fin/buyorrent/BuyFragment.java
+++ Android_Workspace/BuyOrRent/src/aum/fin/buyorrent/BuyFragment.java
@@ -18,15 +18,22 @@ public class BuyFragment  extends Fragment {
     /** (non-Javadoc)
      * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
      */
-    private double dHousePriceMax = 600000;
-    private double dHousePriceMin = 50000;
+    private int mIntHousePriceMax = 600000;
+    private int mIntHousePriceMin = 50000;
+    private int mIntHousePrice    = 300000;
+    
+    private EditText mTextHousePrice;
+    private SeekBar  mSeekHousePrice;
     
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         if (container == null) { //if fragment's container frame does not exist in layout just return null
             return null;
         }
+        
      final ScrollView viewBuy = (ScrollView)inflater.inflate(R.layout.activity_buy, container, false);
+     mTextHousePrice = (EditText) viewBuy.findViewById(R.id.actBuy_editText1);
+     mSeekHousePrice = (SeekBar) viewBuy.findViewById(R.id.actBuy_seekBar1);
      
      //yearly expenses
      final RelativeLayout loYearlyTitle = (RelativeLayout) viewBuy.findViewById(R.id.actBuy_layoutYearlyTitle);
@@ -58,28 +65,57 @@ public class BuyFragment  extends Fragment {
      	}
      } });
      
-     EditText textHousePrice = (EditText) viewBuy.findViewById(R.id.actBuy_editText1);
-     textHousePrice.addTextChangedListener(new TextWatcher() {
+     class BuyTextWatcher implements TextWatcher {
+    	 private int mIntMin = 0;
+    	 private int mIntMax = 0;
+    	 
+    	 public BuyTextWatcher(int iMin, int iMax) {
+    		 mIntMin = iMin;
+    		 mIntMax = iMax;
+    	 }
+    	 
     	 public void afterTextChanged(Editable s) {}	 
     	 public void beforeTextChanged(CharSequence s, int start, int count, int after){}
     	 public void onTextChanged(CharSequence s, int start, int before, int count) {
-    		 SeekBar seekHousePrice = (SeekBar) viewBuy.findViewById(R.id.actBuy_seekBar1);
-    		 seekHousePrice.setMax(1000);
+    		 mSeekHousePrice.setMax(1000);
     		 if(s.toString().length() <= 0)
     		 {
-    			 seekHousePrice.setProgress(0);
+    			 mSeekHousePrice.setProgress(0);
     			 return;
     		 }
     		 double dInput = Double.valueOf(s.toString()).doubleValue();
-    		 double t1 = dInput - dHousePriceMin;
-    		 double t2 = dHousePriceMax - dHousePriceMin;
-    		 double t3 = (t1/t2) * 1000;
-    		 int iPos = (int)(t3 + 0.5);
-    		 seekHousePrice.setProgress(iPos);
+    		 double dTemp = (double)((dInput - mIntMin)/(mIntMax - mIntMin)) * 1000.0;
+    		 int iPos = (int)(dTemp + 0.5);
+    		 mSeekHousePrice.setProgress(iPos);
     	 }
-     }); 
-     textHousePrice.setText("");
      
+     };
+     
+     class BuySeekBarListner implements SeekBar.OnSeekBarChangeListener {
+    	 private int mIntMin = 0;
+    	 private int mIntMax = 0;
+    	 
+    	 public BuySeekBarListner(int iMin, int iMax) {
+    		 mIntMin = iMin;
+    		 mIntMax = iMax;
+    	 }
+    	 
+    	 public void onStartTrackingTouch(SeekBar seekBar) {}
+    	 public void onStopTrackingTouch(SeekBar seekBar) {}
+    	 public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    		 if(fromUser)
+    		 {
+	    		 mIntHousePrice = (int) ((double) (progress/1000.0) * (mIntMax - mIntMin)) + mIntMin;
+	    		 mTextHousePrice.setText(String.valueOf(mIntHousePrice));
+    		 }
+    	 }    	 
+     };
+     
+     mTextHousePrice.addTextChangedListener(new BuyTextWatcher(mIntHousePriceMin, mIntHousePriceMax));
+     mTextHousePrice.setText(String.valueOf(mIntHousePrice));
+     
+     mSeekHousePrice.setOnSeekBarChangeListener(new BuySeekBarListner(mIntHousePriceMin, mIntHousePriceMax));
+          
      return viewBuy;
     }
 }
